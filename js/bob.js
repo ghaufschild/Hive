fa9154ab6243793cc89f10681429197a0d78d724
Garrett Haufschild <garretthaufschild@gmail.com>
2020-01-23 03:29:17 -0500

    Working bob and demo, needs data in index.hmtl specifically.

--- js/bob.js
+++ js/bob.js
@@ -1,4 +1,29 @@
-var bob = {
-    "What is this?":"Hello dude",
-    "Am I insane?":"Yes dude"
+function askWatson() {
+    var question = $("#question").val();
+    question = question.toLowerCase();
+    
+    var chart = new CanvasJS.Chart("chartContainer", {
+      title: {
+        text: "Sentiment for " + question
+      },
+      axisY: {
+        title: "Sentiment"
+      },
+      axisX: {
+        title: "Time",
+        valueFormatString: "MMM",
+        interval: 1,
+        intervalType: "month"
+      },
+      data: [
+      {
+        type: "line",
+        toolTipContent: "<a href = {url}> {title}</a><hr/>Sentiment: {y}",                
+  
+        dataPoints: bob[question]
+      } 
+      ]
+    });
+  
+    chart.render();
 }
